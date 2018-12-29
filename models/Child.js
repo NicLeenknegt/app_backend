a8c15d49c7ec4bdcd2e79c713003238b07c59a75
Nicolaas Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2018-12-29 21:38:30 +0100

    String versie van birthdate aangemaakt

--- models/Child.js
+++ models/Child.js
@@ -26,7 +26,7 @@ let ChildSchema = new mongoose.Schema({
         time:String
     }],
     birthDate: Date,
-    birthDateString:birthDate.toISOString(),
+    birthDateString:String,
     sex:String
 })
 
